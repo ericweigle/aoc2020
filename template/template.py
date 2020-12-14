cc4844aa8eec03e1cb6dad7260566b143a0b8a36
Eric Weigle <eric.weigle@gmail.com>
2020-12-13 22:02:33 -0800

    show an error if you forget the input

--- template/template.py
+++ template/template.py
@@ -6,8 +6,13 @@ import math
 import pprint
 import copy
 
+if len(sys.argv) < 2:
+  print("Usage: %s [filename]" % sys.argv[0])
+  sys.exit(1)
+
+
 for filename in sys.argv[1:]:
-  data = [int(x.strip()) for x in open(filename, 'r').readlines()]
+  data = [x.strip() for x in open(filename, 'r').readlines()]
 
   if len(data) < 100:
     # example
